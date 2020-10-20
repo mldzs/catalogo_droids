e54aa618da836a2f7b396ff1c7d74845cb9a0897
Jonathan Mieldazis <mieldazis@ilhasoft.com.br>
2020-10-20 00:48:28 -0300

    add table 'DemandaPecas' and 'EnderecoEntrega'.

--- catalogo_droids/demanda_pecas/models.py
+++ catalogo_droids/demanda_pecas/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class EnderecoEntrega(models.Model):
+    cep = models.CharField(max_length=255)
+    endereco = models.CharField(max_length=255)
+    bairro = models.CharField(max_length=255)
+    cidade = models.CharField(max_length=255)
+    estado = models.CharField(max_length=255)
+    numero = models.CharField(max_length=255)
+
+
+class DemandaPeca(models.Model):
+    descricao = models.TextField()
+    endereco_entrega = models.ForeignKey(EnderecoEntrega, on_delete=models.CASCADE, related_name='demanda_pecas')
+    telefone = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    anunciante = models.ForeignKey(User, on_delete=models.PROTECT, related_name='demanda_pecas')
+    status_finalizacao = models.CharField(max_length=255)
