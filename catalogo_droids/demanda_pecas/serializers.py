f71d6a53c37b53d0451aa07696341e1604ef17cb
Jonathan Mieldazis <mieldazis@ilhasoft.com.br>
2020-10-21 00:29:23 -0300

    create serializer

---
+++ catalogo_droids/demanda_pecas/serializers.py
@@ -0,0 +1,33 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+from .models import EnderecoEntrega, DemandaPeca
+
+
+class EnderecoEntregaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EnderecoEntrega
+        fields = '__all__'
+
+
+class DemandaPecaSerializer(serializers.ModelSerializer):
+    endereco_entrega = EnderecoEntregaSerializer()
+    anunciante = serializers.SlugRelatedField(slug_field='username', queryset=User.objects.all())
+
+    class Meta:
+        model = DemandaPeca
+        fields = '__all__'
+
+    def create(self, validated_data):
+        endereco_entrega = validated_data.pop('endereco_entrega')
+        obj_endereco_entrega = EnderecoEntrega.objects.create(**endereco_entrega)
+
+        demanda_peca = DemandaPeca.objects.create(endereco_entrega=obj_endereco_entrega, **validated_data)
+        return demanda_peca
+
+    def update(self, instance, validated_data):
+        endereco_entrega = validated_data.pop('endereco_entrega')
+        obj_endereco_entrega, created = EnderecoEntrega.objects.get_or_create(**endereco_entrega)
+
+        demanda_peca = DemandaPeca.objects.update(endereco_entrega=obj_endereco_entrega, **validated_data)
+        return demanda_peca
