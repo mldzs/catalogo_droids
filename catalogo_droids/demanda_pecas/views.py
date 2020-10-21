ce5ddedaeb82023faf8c99d87ac72bdba2d8ad92
Jonathan Mieldazis <mieldazis@ilhasoft.com.br>
2020-10-21 00:55:35 -0300

    add jwt login, filter django and viewset

--- catalogo_droids/demanda_pecas/views.py
+++ catalogo_droids/demanda_pecas/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .models import DemandaPeca
+from .serializers import DemandaPecaSerializer
+from ..permissions import IsUsuarioAdministradorOuUsuarioDono
+from ..util_views import MixedPermissionModelViewSet
+
+
+class DemandaPecaViewSet(MixedPermissionModelViewSet):
+    queryset = DemandaPeca.objects.all()
+    serializer_class = DemandaPecaSerializer
+    #permission_classes = [IsAuthenticated]
+    filterset_fields = ["descricao", "endereco_entrega__cep", "email", "anunciante__username"]
+    search_fields = ["descricao", "endereco_entrega__cep", "email", "anunciante__username"]
+    ordering_fields = ["descricao", "endereco_entrega__cep", "email", "anunciante__username"]
+
+    http_method_names = ["get", "post", "put", "delete"]
+
+    permission_classes_by_action = {
+        "update": [IsAuthenticated, IsUsuarioAdministradorOuUsuarioDono],
+        "destroy": [IsAuthenticated, IsUsuarioAdministradorOuUsuarioDono]
+    }
+
+    def get_queryset(self):
+        todas_demanda_pecas = super().get_queryset()
+        user = self.request.user
+
+        if not user.is_superuser:
+            todas_demanda_pecas = todas_demanda_pecas.filter(anunciante=user)
+
+        return todas_demanda_pecas
--- catalogo_droids/settings.py
+++ catalogo_droids/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -42,6 +42,12 @@ INSTALLED_APPS = [
 # my apps
 INSTALLED_APPS += [
     'catalogo_droids.demanda_pecas',
+]
+
+# system libs
+INSTALLED_APPS += [
+    "rest_framework",
+    "django_filters",
 ]
 
 MIDDLEWARE = [
@@ -123,3 +129,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# DRF
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": ["rest_framework_simplejwt.authentication.JWTAuthentication"],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+    "DEFAULT_FILTER_BACKENDS": [
+        "django_filters.rest_framework.DjangoFilterBackend",
+        "rest_framework.filters.SearchFilter",
+        "rest_framework.filters.OrderingFilter",
+    ],
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+}
